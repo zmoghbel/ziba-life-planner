94cf31bff6685c4972deb2c86f749087a17e47a0
Zahra Adelung <zahra.adelung@gmail.com>
2020-09-16 11:49:42 +0200

    update splashPage, homePage bg, shared preferences package

--- lib/pages/home-page.dart
+++ lib/pages/home-page.dart
@@ -8,40 +8,71 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   CalendarController _controller;
+  Map<DateTime, List<dynamic>> _events;
 
   @override
   void initState() {
-    // TODO implement initState
     super.initState();
     _controller = CalendarController();
+    _events = {};
+  }
+
+  Map<String, dynamic> encodeMap(Map<DateTime, dynamic> map) {
+    Map<String, dynamic> newMap = {};
+    map.forEach((key, value) {
+      newMap[key.toString()] = map[key];
+    });
+    return newMap;
+  }
+
+  Map<DateTime, dynamic> decodeMap(Map<String, dynamic> map) {
+    Map<DateTime, dynamic> newMap = {};
+    map.forEach((key, value) {
+      newMap[DateTime.parse(key)] = map[key];
+    });
+    return newMap;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       //backgroundColor: Colors.white,
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+      body: Stack(
         children: [
-          TableCalendar(
-            calendarController: _controller,
-            onDaySelected: (date, event) {
-              print(
-                date.toIso8601String(),
-              );
-            },
-            headerStyle: HeaderStyle(
-              formatButtonShowsNext: false,
-            ),
-            startingDayOfWeek: StartingDayOfWeek.monday,
-            calendarStyle: CalendarStyle(
-              todayStyle: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 18.0,
-                color: Colors.green[600],
-              ),
-              todayColor: Color(0xFFFFFFFF),
-              selectedColor: Theme.of(context).primaryColor,
+          Image(
+            image: AssetImage('assets/img/background.png'),
+            fit: BoxFit.fill,
+            height: double.infinity,
+            width: double.infinity,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(30.0),
+            child: Column(
+              //crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                TableCalendar(
+                  events: _events,
+                  calendarController: _controller,
+                  onDaySelected: (date, event) {
+                    print(
+                      date.toIso8601String(),
+                    );
+                  },
+                  headerStyle: HeaderStyle(
+                    formatButtonShowsNext: false,
+                  ),
+                  startingDayOfWeek: StartingDayOfWeek.monday,
+                  calendarStyle: CalendarStyle(
+                    todayStyle: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18.0,
+                      color: Colors.green[600],
+                    ),
+                    todayColor: Color(0xFFFFFFFF),
+                    selectedColor: Theme.of(context).primaryColor,
+                  ),
+                ),
+              ],
             ),
           ),
         ],
