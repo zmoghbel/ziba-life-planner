aed1668fe50769a4d41bf2844cd963c263ac52e8
Zahra Adelung <zahra.adelung@gmail.com>
2020-10-20 14:10:04 +0200

    Adding app-bar

--- lib/pages/add-task.dart
+++ lib/pages/add-task.dart
@@ -10,18 +10,26 @@ class _AddTaskState extends State<AddTask> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Add a new task',
+        ),
+      ),
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20.0),
           child: Container(
+            margin: EdgeInsets.only(top: 10),
             child: Column(
               children: [
                 Row(
                   children: [
-                    TextField(
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'Title',
+                    Expanded(
+                      child: TextField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Title',
+                        ),
                       ),
                     ),
                     IconButton(
@@ -32,34 +40,44 @@ class _AddTaskState extends State<AddTask> {
                         }),
                   ],
                 ),
-                DropdownButton(
-                  value: dropdownValue,
-                  onChanged: (String newValue) {
-                    setState(() {
-                      dropdownValue = newValue;
-                    });
-                  },
-                  items: <String>[
-                    'A1',
-                    'A2',
-                    'A3',
-                    'A4',
-                    'B1',
-                    'B2',
-                    'B3',
-                    'B4',
-                    'C1',
-                    'C2',
-                    'C3',
-                    'D1',
-                    'D2',
-                  ].map<DropdownMenuItem<String>>((String value) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(value),
-                    );
-                  }).toList(),
-                ),
+                Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                  Text(
+                    'Priority',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                  SizedBox(width: 50),
+                  DropdownButton(
+                    value: dropdownValue,
+                    onChanged: (String newValue) {
+                      setState(() {
+                        dropdownValue = newValue;
+                      });
+                    },
+                    items: <String>[
+                      'A1',
+                      'A2',
+                      'A3',
+                      'A4',
+                      'B1',
+                      'B2',
+                      'B3',
+                      'B4',
+                      'C1',
+                      'C2',
+                      'C3',
+                      'D1',
+                      'D2',
+                    ].map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                  ),
+                ]),
               ],
             ),
           ),
